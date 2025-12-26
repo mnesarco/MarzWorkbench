e5f838008f81676e5e85ba461ce39210cda236a2
ᴩʜᴏɴᴇᴅʀᴏɪᴅ <73050054+PhoneDroid@users.noreply.github.com>
2025-12-26 11:08:17 -0500

    Update Licensing Headers
    * Remove Python 2 UTF-8 annotations
    * Add SPDX License Identifiers
    * Add SPDX File Notices
    * Update Copyright Headers
    * Correct manifest license Id
    * Add Missing License Text
    * Upgrade GPL-2 to GPL-3

--- Resources/inkscape/marz/meta.py
+++ Resources/inkscape/marz/meta.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
-# coding=utf-8
-#
-# Copyright (c) 2024 - Frank D. Martínez M.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileNotice: Part of the Marz addon.
+
+################################################################################
+#                                                                              #
+#   Copyright (c) 2024 Frank David Martínez Muñoz <mnesarco at gmail.com>      #
+#                                                                              #
+#   This program is free software: you can redistribute it and / or            #
+#   modify it under the terms of the GNU General Public License as             #
+#   published by the Free Software Foundation, either version 3 of             #
+#   the License, or (at your option) any later version.                        #
+#                                                                              #
+#   This program is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                       #
+#                                                                              #
+#   See the GNU General Public License for more details.                       #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.      #
+#                                                                              #
+################################################################################
 
 """
 API to generate .inx files and cli arguments directly from python code in
--- freecad/marz/utils/colors.py
+++ freecad/marz/utils/colors.py
@@ -1,22 +1,25 @@
-# -*- coding: utf-8 -*-
-# +---------------------------------------------------------------------------+
-# |  Copyright (c) 2020 Frank Martinez <mnesarco at gmail.com>                |
-# |                                                                           |
-# |  This file is part of Marz Workbench.                                     |
-# |                                                                           |
-# |  Marz Workbench is free software: you can redistribute it and/or modify   |
-# |  it under the terms of the GNU General Public License as published by     |
-# |  the Free Software Foundation, either version 3 of the License, or        |
-# |  (at your option) any later version.                                      |
-# |                                                                           |
-# |  Marz Workbench is distributed in the hope that it will be useful,        |
-# |  but WITHOUT ANY WARRANTY; without even the implied warranty of           |
-# |  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
-# |  GNU General Public License for more details.                             |
-# |                                                                           |
-# |  You should have received a copy of the GNU General Public License        |
-# |  along with Marz Workbench.  If not, see <https://www.gnu.org/licenses/>. |
-# +---------------------------------------------------------------------------+
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileNotice: Part of the Marz addon.
+
+################################################################################
+#                                                                              #
+#   Copyright (c) 2020 Frank David Martínez Muñoz <mnesarco at gmail.com>      #
+#                                                                              #
+#   This program is free software: you can redistribute it and / or            #
+#   modify it under the terms of the GNU General Public License as             #
+#   published by the Free Software Foundation, either version 3 of             #
+#   the License, or (at your option) any later version.                        #
+#                                                                              #
+#   This program is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                       #
+#                                                                              #
+#   See the GNU General Public License for more details.                       #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.      #
+#                                                                              #
+################################################################################
 
 from typing import Tuple
 
