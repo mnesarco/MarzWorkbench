e336cb06a2573ca5d75375dbe170be7a6e1d7df6
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2025-12-26 10:46:18 -0500

    Upgrade GPL-2 to GPL-3

--- Resources/inkscape/marz/meta.py
+++ Resources/inkscape/marz/meta.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileNotice: Part of the Marz addon.
 
 ################################################################################
@@ -8,7 +8,7 @@
 #                                                                              #
 #   This program is free software: you can redistribute it and / or            #
 #   modify it under the terms of the GNU General Public License as             #
-#   published by the Free Software Foundation, either version 2 of             #
+#   published by the Free Software Foundation, either version 3 of             #
 #   the License, or (at your option) any later version.                        #
 #                                                                              #
 #   This program is distributed in the hope that it will be useful,            #
